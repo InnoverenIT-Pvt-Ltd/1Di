9eda2fe604a63797296818edb3edd2d573fb6a39
Dibya391 <dibyaranjan828@gmail.com>
2024-10-02 18:48:08 +0530

    drb1

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -1,12 +1,12 @@
 //KORERO
-const login_url = "https://develop.tekorero.com/testHr";
-const base_url = "https://develop.tekorero.com/testHr/api/v1";
-const base_url2 = "https://develop.tekorero.com/testErp";
+// const login_url = "https://develop.tekorero.com/testHr";
+// const base_url = "https://develop.tekorero.com/testHr/api/v1";
+// const base_url2 = "https://develop.tekorero.com/testErp";
 
 // NUBOX
-// const login_url = "https://develop.tekorero.com/employeePortal";
-// const base_url = "https://develop.tekorero.com/employeePortal/api/v1";
-// const base_url2 = "https://develop.tekorero.com/Erp";
+const login_url = "https://1DiWeb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com/";
+const base_url = "https://1DiWeb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com/api/v1";
+const base_url2 = "https://1DiWeb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com/Erp";
 
 
 export { base_url, base_url2, login_url };
