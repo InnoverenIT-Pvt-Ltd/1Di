90d9e1c8946511814386871cbf6eed72a9dc77e8
Dibya391 <dibyaranjan828@gmail.com>
2024-10-09 16:53:32 +0530

    drb1

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -4,9 +4,14 @@
 // const base_url2 = "https://develop.tekorero.com/testErp";
 
 // 1 Di B2B
-const login_url = "http://1diweb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com";
-const base_url = "http://1diweb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com/api/v1";
-const base_url2 = "http://1dierp-env.eba-ed4pbxag.us-east-2.elasticbeanstalk.com";
+const login_url = "https://webbackend.1di.ca";
+const base_url = "https://webbackend.1di.ca/api/v1";
+const base_url2 = "https://erpbackend.1di.ca";
+
+//old
+// const login_url = "http://1diweb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com";
+// const base_url = "http://1diweb-env.eba-5ivfdpyc.us-east-2.elasticbeanstalk.com/api/v1";
+// const base_url2 = "http://1dierp-env.eba-ed4pbxag.us-east-2.elasticbeanstalk.com";
 
 export { base_url, base_url2, login_url };
 
